0ef3e41ccdb98c63acd1b3b37745b6e4fc8211a7
Christopher A. Sesugh <codingsimba@Christophers-MacBook-Pro.local>
2024-07-19 18:00:06 +0100

    change course name

--- courses/types.ts
+++ courses/types.ts
@@ -1,15 +1,18 @@
 export interface Course {
   title: string;
   id: string;
+  slug: string;
   published: boolean;
   modules?: Module[];
 }
 export interface Module {
   title: string;
   id: string;
+  slug: string;
   lessons?: Lesson[];
 }
 export interface Lesson {
   title: string;
   id: string;
+  slug: string;
 }
